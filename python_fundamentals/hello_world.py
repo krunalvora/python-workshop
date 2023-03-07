35cef6aea8f9496fecde74d41999230700c6d79b
Krunal Vora <krunalvora4@gmail.com>
2023-03-07 01:33:50 +0000

    update

--- python_fundamentals/hello_world.py
+++ python_fundamentals/hello_world.py
@@ -2,5 +2,19 @@
 Python fundamentals
 """
 
-print("Hello Python world!")
+message = "   Hello Python world!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = f"{first_name} {last_name}"
+
+greeting = f"Hello, {full_name.title()}!"
+
+print(greeting)
+
+print(message.strip())
+
+print(greeting.removeprefix('Hello'))
 
